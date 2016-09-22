8caf29d601dddb6f48b1e488b97b8d5714d78eab
ryang <decatf@gmail.com>
2016-09-21 23:16:44 -0400

    Revert "Remove obsolete debug option"
    This reverts commit a672f6ba4c9f65de0b94bcdc639f1e053d7ee5d9.

--- libs/hwui/Properties.cpp
+++ libs/hwui/Properties.cpp
@@ -33,6 +33,7 @@ bool Properties::debugLayersUpdates = false;
 bool Properties::debugOverdraw = false;
 bool Properties::showDirtyRegions = false;
 bool Properties::skipEmptyFrames = true;
+bool Properties::swapBuffersWithDamage = true;
 bool Properties::useBufferAge = true;
 bool Properties::enablePartialUpdates = true;
 
@@ -141,6 +142,7 @@ bool Properties::load() {
     debugLevel = (DebugLevel) property_get_int(PROPERTY_DEBUG, kDebugDisabled);
 
     skipEmptyFrames = property_get_bool(PROPERTY_SKIP_EMPTY_DAMAGE, true);
+    swapBuffersWithDamage = property_get_bool(PROPERTY_SWAP_WITH_DAMAGE, true);
     useBufferAge = property_get_bool(PROPERTY_USE_BUFFER_AGE, true);
     enablePartialUpdates = property_get_bool(PROPERTY_ENABLE_PARTIAL_UPDATES, true);
 
